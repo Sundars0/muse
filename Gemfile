25f929d20447da4f6a81606b0098e72e98bb2900
sundars0 <sundars04@rediffmail.com>
2016-11-05 15:11:52 +0000

    add gem acts_as_votable and like and dislike functionality

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'simple_form'
 gem 'devise'
 gem "paperclip", "~> 5.0.0"
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'acts_as_votable', '~> 0.10.0'
 gem 'sqlite3'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root :to => 'posts#index'
 
   resources :posts do
+    member do
+      get "like", to: "posts#upvote"
+      get "dislike", to: "posts#downvote"
+    end
     resources :comments
   end
 end
